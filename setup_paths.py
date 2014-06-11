e207827c192299c02e8e328aec714c407330934d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-06-11 13:56:50 +0200

    added a silly script to build the appropriate paths

---
+++ setup_paths.py
@@ -0,0 +1,16 @@
+import os, errno
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+from allpaths import datapath, datapath_w51, analysis_figurepath, figurepath, datapath_spectra, regionpath
+
+for path in (datapath, datapath_w51, analysis_figurepath, figurepath,
+             datapath_spectra, regionpath):
+    mkdir_p(path)
