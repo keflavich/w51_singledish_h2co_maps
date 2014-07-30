fix CO cube stitching weighting scheme

analysis_scripts/integrated_h2co_properties.py
@@ -78,18 +78,19 @@ for label,vrange in (("",[40,75]), ("lower",[40,62]), ("upper",[62,75])):
     h2co22peak = h2co22slab.with_mask(boolean_maskslab).min(axis=0)
     h2co11_13peak = h2co11_13slab.with_mask(boolean_maskslab).min(axis=0)
 
-    depth11 = -np.log((h2co11peak+cont11) / cont11)
-    depth22 = -np.log((h2co22peak+cont22) / cont22)
+    depth11 = -np.log((h2co11peak.value+cont11) / cont11)
+    depth22 = -np.log((h2co22peak.value+cont22) / cont22)
 
     hdu.header['BUNIT'] = ''
-    hdu.data = depth11.value
+    hdu.data = depth11
     hdu.writeto(dpath(label+'peak_optdepth_11.fits'), clobber=True)
-    hdu.data = depth22.value
+    hdu.data = depth22
     hdu.writeto(dpath(label+'peak_optdepth_22.fits'), clobber=True)
-    hdu.data = (depth11/depth22).value
+    hdu.data = (depth11/depth22)
     hdu.writeto(dpath(label+'peak_optdepth_ratio.fits'), clobber=True)
 
-    h2co11integ.hdu.writeo(dpath(label+"_H213CO_SNmasked_integrated.fits", clobber=True))
+    hdu.data = h2co11_13integ.value
+    hdu.writeto(dpath(label+"H213CO_SNmasked_integrated.fits"), clobber=True)
 
     # These are not particularly useful
     # Better to try and measure a T_ex per pixel

reduction_scripts/stitch_grs_cubes.py
@@ -56,7 +56,10 @@ stacked = fits.PrimaryHDU()
 # cast out of boolean
 addnan = lambda x,y: np.add(np.nan_to_num(x),np.nan_to_num(y),dtype='float')
 add = lambda x,y: np.add(x,y,dtype='float')
-stacked.data = reduce(addnan, (r1.data, r2.data, r3.data))/reduce(add, [(r1.data != 0), (r2.data != 0), (r3.data != 0)])
+weights = reduce(add, [((r1.data[212,:,:] != 0) & (np.isfinite(r1.data[212,:,:]))),
+                       ((r2.data[212,:,:] != 0) & (np.isfinite(r2.data[212,:,:]))),
+                       ((r3.data[212,:,:] != 0) & (np.isfinite(r3.data[212,:,:])))])
+stacked.data = reduce(addnan, (r1.data, r2.data, r3.data))/weights
 stacked.header = outheader
 stacked.writeto(paths.dpath2('grs_48and50_cube.fits'), clobber=True)
 