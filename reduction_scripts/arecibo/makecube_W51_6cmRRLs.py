6ab7747e2a0afea9cbba44c98b51a464cab1d090
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-30 10:33:32 +0200

    more refactoring / cleanup

--- reduction_scripts/arecibo/makecube_W51_6cmRRLs.py
+++ reduction_scripts/arecibo/makecube_W51_6cmRRLs.py
@@ -15,69 +15,8 @@ for line in bsgs:
     linename_ha = linename = line['linename']
     if linefreq == 0:
         continue
-    # debug
-    #if linename.lower() != 'h112ab':
-    #    continue
-
-    vmin = 30
-    vmax = 90
-    velocityrange = [-150,300]
-
-    #makecube.generate_header(49.523158,-0.34987466,naxis1=96,naxis2=96,pixsize=20,naxis3=1600,cd3=0.5,clobber=True,restfreq=4.8296594e9)
-    #makecube.generate_header(49.353568,-0.2982199,naxis1=144,naxis2=96,pixsize=20,naxis3=1600,cd3=0.5,clobber=True,restfreq=4.8296594e9)
-    # reduced to CD3 = 1.0, naxis3 = 350 because of size and because the arecibo spectra looked artificially smoothed
-    cd3 = 1.0
-    naxis3 = int((velocityrange[1]-velocityrange[0]) / cd3)
-    # Updated June 18, 2014 to use smaller pixels for more useful comparison to other data
-    makecube.generate_header(49.209553, -0.277137, naxis1=308, naxis2=205,
-                             pixsize=15, naxis3=int(naxis3), cd3=cd3,
-                             crval3=50.0, clobber=True, author='Adam Ginsburg')
-    cubename = '/Users/adam/work/h2co/maps/w51/W51_%slpha_cube' % linename
-    makecube.make_blank_images(cubename,clobber=True)
-
-    for date in ('0910','0911','0912','0915',):
-        fn = '/Users/adam/observations/arecibo/2012{date}/W51_{line}_spectra_{date}.fits'.format(line=linename,date=date)
-        if not os.path.exists(fn):
-            fn = '/Users/adam/observations/arecibo/2012{date}/W51_{line}_spectra_{date}.fits'.format(line=linename.replace("h","H"),date=date)
-        if not os.path.exists(fn):
-            fn = '/Users/adam/observations/arecibo/20120910/W51_{line}_spectra_{date}.fits'.format(line=linename.replace("h","H"),date=date)
-        log.info(" ".join([str(x) for x in (fn, velocityrange, linename, linefreq)]))
-        if os.path.exists(fn):
-            makecube.add_file_to_cube(fn,
-                                      cubename+'.fits',
-                                      nhits=cubename+'_nhits.fits',
-                                      velocityrange=velocityrange,
-                                      excludefitrange=[vmin,vmax], 
-                                      progressbar=True,
-                                      add_with_kernel=True,
-                                      kernel_fwhm=20./3600.,
-                                      chmod=True,
-                                      linefreq=linefreq)
-        else:
-            raise IOError("Did not find file {0}".format(fn))
-    #    f = pyfits.open(fn)
-    #
-    #    fixed=False
-    #    for kw in ('TDIM%i' % ii for ii in xrange(5)):
-    #        if kw in f[0].header:
-    #            fixed = True
-    #            del f[0].header[kw]
-    #    if fixed:
-    #        f[0].writeto(fn.replace(".fits","_fixed.fits"),output_verify='fix')
-    #        fn = fn.replace(".fits","_fixed.fits")
-
-
-    flat_vrange = [30,90]
-
-    if os.path.exists(cubename+".fits"):
-        makecube.runscript(cubename)
-        makecube.make_flats(cubename,vrange=flat_vrange,noisevrange=[-50,-1])
-        makecube.make_flats(cubename,vrange=[-100,-40],noisevrange=[-50,-1],
-                            out_suffix='_Helium')
-        #makecube.make_flats(cubename,vrange=flat_vrange,noisevrange=[-50,-1],suffix='.fits')
-    else:
-        print "Failure at ",line
         
+    # 7/29/2014: remove non-superresolution maps
     # 1/10/2014: add "superresolution" maps
     vmin = 30
     vmax = 90
