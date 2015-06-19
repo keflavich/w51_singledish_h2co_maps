c91f88866f3778adb64714645c2a7442f0510093
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-19 15:41:27 +0200

    messing with clusters and column density pdfs

--- analysis_scripts/cluster_properties.py
+++ analysis_scripts/cluster_properties.py
@@ -2,14 +2,40 @@ import numpy as np
 import imf
 import pylab as pl
 from astropy.utils.console import ProgressBar
+pl.matplotlib.rc_file('pubfiguresrc')
 
 nstars_per_cluster = 100
+maxmass = 3e4
 
-cl_masses = np.logspace(3,np.log10(2e4))
-clusters = np.array([[imf.make_cluster(mass,silent=True) for ii in range(nstars_per_cluster)] for mass in ProgressBar(cl_masses)])
-luminosities = [imf.lum_of_cluster(cl) for section in clusters for cl in section]
-cl_masses_flat = np.array([x for mass in cl_masses for x in [mass for ii in range(nstars_per_cluster)]])
+cl_masses = np.logspace(2.5,np.log10(maxmass))
+clusters = np.array([[imf.make_cluster(mass*(np.random.randn()/20.+1.),silent=True) for ii in range(nstars_per_cluster)] for mass in ProgressBar(cl_masses)])
+luminosities = [imf.lum_of_cluster(cl) for section in ProgressBar(clusters) for cl in section]
+#cl_masses_flat = np.array([x for mass in cl_masses for x in [mass for ii in range(nstars_per_cluster)]])
+cl_masses_flat = np.array([sum(cl) for section in clusters for cl in section])
 n_ostars = np.array([(cl>8).sum() for section in clusters for cl in section])
+
+# guess, not measurement
+nostars_w51 = n_ostars[(cl_masses_flat > 4e3) & (cl_masses_flat < 9e3)]
+
+
+pl.figure(6).clf()
+pl.semilogx(cl_masses_flat, luminosities, '.', alpha=0.5)
+pl.axhline(np.log10(8.3e6), linewidth=10, alpha=0.1, zorder=-5)
+pl.axhline(np.log10(2e7), linewidth=10, alpha=0.1, zorder=-5)
+pl.xlim(10**2.5, (maxmass))
+pl.ylim(5.5, 8.1)
+pl.xlabel("Cluster mass ($M_\odot$)")
+pl.ylabel("Cluster luminosity (log $L_\odot$)")
+pl.savefig("cluster_mass_vs_luminosity.png")
+
+pl.figure(9).clf()
+pl.loglog(cl_masses_flat, n_ostars, '.', alpha=0.5)
+pl.xlim(10**2.5, (maxmass))
+pl.ylim(0, n_ostars.max())
+pl.xlabel("Cluster mass ($M_\odot$)")
+pl.ylabel("Number of OB-stars")
+pl.savefig("cluster_mass_vs_n_ostars.png")
+
 
 def lnprob(mass, luminosity=2e7, luminosity_error=5e6):
     if mass < 1:
@@ -34,12 +60,6 @@ pos,prob,state,blobs = sampler.run_mcmc(p0, 400)
 
 
 
-pl.figure(6).clf()
-pl.semilogx(cl_masses_flat, luminosities, '.', alpha=0.5)
-pl.axhline(np.log10(8.3e6), linewidth=10, alpha=0.1, zorder=-5)
-pl.axhline(np.log10(2e7), linewidth=10, alpha=0.1, zorder=-5)
-
-#nostars_w51 = n_ostars[(cl_masses_flat > 4e3) & (cl_masses_flat < 9e3)]
 
 pl.figure(7).clf()
 pl.hist(nostars_w51, bins=30)
--- plot_scripts/column_pdf.py
+++ plot_scripts/column_pdf.py
@@ -7,6 +7,7 @@ import os
 matplotlib.rc_file(paths.rcfilepath)
 
 d = fits.getdata(paths.dpath2('HIGAL_W51_mosaic_fit_070to500_N.fits'))
+f = fits.open(paths.dpath2('HIGAL_W51_mosaic_fit_070to500_N.fits'))
 pl.clf()
 X = pl.hist(np.log10(d[d==d]), bins=50, histtype='stepfilled', linewidth=3,
             edgecolor='k', facecolor=(0,0,0,0.5), log=True)
@@ -18,3 +19,17 @@ pl.xlabel("Column Density N(H$_2$)")
 pl.ylabel("Number of pixels")
 ax.set_xlim(20.95, 23.22)
 pl.savefig(paths.fpath("HiGal_ColumnDensityHistogram.pdf"))
+
+import pyregion
+regs = pyregion.open(paths.rpath('main_and_b_squares.reg'))
+
+reg1 = pyregion.ShapeList([regs[0]])
+reg2 = pyregion.ShapeList([regs[1]])
+mask1 = reg1.get_mask(f[0])
+mask2 = reg2.get_mask(f[0])
+
+X = pl.hist(np.log10(d[mask1]), bins=50, histtype='stepfilled', linewidth=3,
+            edgecolor='g', facecolor=(0,1,0,0.5), log=True)
+X = pl.hist(np.log10(d[mask2]), bins=50, histtype='stepfilled', linewidth=3,
+            edgecolor='b', facecolor=(0,0,1,0.5), log=True)
+pl.savefig(paths.fpath("HiGal_ColumnDensityHistogram_mainandb.pdf"))
